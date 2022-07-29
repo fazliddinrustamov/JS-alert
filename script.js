1227418600755a4599044b4c2c8e366ef3a801ce
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-07-29 10:53:20 +0500

    edit as party

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-var intro = alert("Welcome to our site!");
+var intro = alert("Welcome to our party!");
 var first_name = prompt("What is your name?", "Fazliddin").trim();
-var surname = prompt("What is your last name?", "Rustamov").trim();
-var age = prompt("How old are you?", "15 years old").trim();
-var profession = prompt("What is your profession?", "Pupil").trim();
-var home = prompt("Where do you live?", "Tashkent").trim();
+var age = prompt("How old are you?", "15").trim();
+var minAge = 18;
+if (age >= minAge) {
+  alert ("You are welcome!")
+} else {
+  alert ("Do not be discouraged, you will come when you grow up!")
+}
 var conclusion =  alert("Thanks for your choice");
 
-console.log(`Full name: ${first_name} ${surname}.`);
+console.log(`First name: ${first_name}`);
 console.log(`Age: ${age}.`);
-console.log(`Profession: ${profession}.`);
-console.log(`Location: ${home}.`);
-
-console.log(`Full name: ${first_name} ${surname}.
-Age: ${age}.
-Profession: ${profession}.
-Location: ${home}.`);
-
-console.log("First name " + first_name + ", Last name " + surname + "\n" + "Profession: " + profession + "\n" + "Age: " + age + "\n" + "Location: " + home);
